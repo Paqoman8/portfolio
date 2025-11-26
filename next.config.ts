940414eb1e6d12f1e67e4ab0dd0c3117380b4db3
Paqoman8 <joel.houinsavi@epitech.eu>
2025-11-26 22:01:11 +0100

    feat: Add static export configuration and deployment scripts for GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+
+const isProd = process.env.NODE_ENV === "production";
+const repo = "portfolio"; // ton nom de repo GitHub
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
